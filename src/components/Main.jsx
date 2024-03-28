ff7877b5eec222581f53d38b95844b1e69a5d957
VARUN YADAV <64697068+varunyadav1175@users.noreply.github.com>
2024-03-29 01:48:35 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,7 +26,7 @@ const Main = () => {
               </a>
             </li>
             <li className="transition-all duration-500 translate-y-0 motion-reduce:transition-none opacity-1">
-              <a href="mailto:varunyadav1175@gmail.com" target="_blank">
+              <a href="https://mail.google.com/mail/?view=cm&fs=1&to=varunyadav1175@gmail.com&su=Invitation%20To%20Connect" target="_blank">
                 <SiGmail className="w-8 h-8 text-black transition duration-300 ease-out svg-inline--fa fa-linkedin md:h-12 md:w-12 hover:-translate-y-1 motion-reduce:hover:translate-y-0 dark:text-slate-300" />
               </a>
             </li>
